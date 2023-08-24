2259867182cc5eaccb42314790e10fab6e1bfbb2
HrantSahakyan <hrant.sahakyan04@gmail.com>
2023-08-25 00:54:21 +0400

    [ADD] Basic showing of data from db

---
+++ GaussClub/Controllers/UniversityController.cs
@@ -0,0 +1,21 @@
+﻿using GaussClub.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GaussClub.Controllers
+{
+    public class UniversityController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UniversityController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var universities = _context.Universities.ToList();
+            return View(universities);
+        }
+    }
+}
