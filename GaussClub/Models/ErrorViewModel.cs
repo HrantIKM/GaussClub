68f76886079470ed0749cc644852a251efe7f56e
HrantSahakyan <hrant.sahakyan04@gmail.com>
2023-08-24 14:04:03 +0400

    Add project files.

---
+++ GaussClub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GaussClub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
